e05bfa959661cb292dba70487d2157cbbec278a0
Julien <julien.cullen@tcs.com>
2019-10-09 10:59:34 -0400

    Initial Changes, redone.
    Accidentally deleted my changes...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,40 @@
+import { HomeComponent } from './home/home.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //Whenever we add a Component to a path here, you must add it to the array "routingComponents"
+  //    at the bottom of the page.
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent}
+
+
+
+
+  //Not currently in use:
+  /*
+  ,
+  { path: 'addProfile', component: AddProfileComponent },
+  { path: 'addEvaluation', component: AddEvaluationComponent },
+
+  { path: 'getProfile/:id', component: GetProfileComponent },
+  { path: 'getEvaluation/:id', component: GetEvaluationComponent },
+
+  { path: 'listProfile', component: ListProfileComponent },
+  { path: 'listEvaluation', component: ListEvaluationComponent },
+
+  { path: 'updateProfile/:id', component: UpdateProfileComponent },
+  { path: 'updateEvaluation/:id', component: UpdateEvaluationComponent }
+  */
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
+export const routingComponents = [HomeComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
